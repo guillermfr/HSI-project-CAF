38549e5b746e5c29f1be2d91474545ca68df7ba7
guillermfr <guillermfr@cy-tech.fr>
2026-02-26 18:42:41 +0100

    :construction: Ajout des définitions pour les durées d'acquittement et de timer

--- lib/templates/define.h
+++ lib/templates/define.h
@@ -28,4 +28,19 @@
  */
 #define CMD_ETEINTE 0u
 
+/**
+ * @brief Durée maximale pour l'acquittement dans la FSM des feux classiques
+ */
+#define ACQ_FEUX_CLASSIQUES 1.0
+
+/**
+ * @brief Durée maximale pour l'acquittement dans la FSM des clignotants et warnings
+ */
+#define ACQ_FEUX_CLIGNOTANTS_WARNINGS 1.0
+
+/**
+ * @brief Durée maximale pour le timer dans la FSM des essuie-glaces et du lave glace 
+ */
+#define TIMER_ESSUIE_GLACES_LAVE_GLACE 2.0
+
 #endif /* DEFINE_H */
