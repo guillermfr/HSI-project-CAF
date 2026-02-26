02ab7b057f0a22269a26532c4fa7a2fedc9cc8d0
guillermfr <guillermfr@cy-tech.fr>
2026-02-26 18:33:18 +0100

    :construction: :bug: Ajout de commentaires doxygen pour les callback et corrections d'erreurs

--- fsm/fsm_feux_classiques.c
+++ fsm/fsm_feux_classiques.c
@@ -45,27 +45,42 @@ typedef enum {
     EV_ERR = 255
 } fsm_event_t;
 
+/**
+ * @brief Callback appelé lors de la transition d'initialisation de la FSM.
+ */
 static int callback_initialisation(void) {
     printf("[FSM] -> INITIALISATION\n");
     return 0;
 }
 
+/**
+ * @brief Callback appelé lors de la transition où les feux passent de l'état éteints à allumés.
+ */
 static int callback_allumer_feux(void) {
     printf("[FSM] -> ALLUMER FEUX\n");
     g_allumes_debut_ms = time(NULL); 
     return 0;
 }
 
+/**
+ * @brief Callback appelé lors de la transition après avoir reçu l'acquittement.
+ */
 static int callback_acquitter(void) {
     printf("[FSM] -> ACQUITTER\n");
     return 0;
 }
 
+/**
+ * @brief Callback appelé lors de la transition où les feux passent de l'état allumés à éteints.
+ */
 static int callback_eteindre_feux(void) {
     printf("[FSM] -> ETEINDRE FEUX\n");
     return 0;
 }
 
+/**
+ * @brief Callback appelé lorsqu'il y a une erreur.
+ */
 static int callback_erreur(void) {
     printf("[FSM] -> ERREUR\n");
     return 0;
@@ -81,6 +96,8 @@ typedef struct {
 
 /* Transition table */
 tTransition trans[] = {
+
+    /* INITIALISATION */
     {ST_INIT, EV_INIT, &callback_initialisation, ST_ETEINTS},
 
     /* ETEINTS */
@@ -93,11 +110,11 @@ tTransition trans[] = {
     {ST_ALLUMES, EV_ACQUITTEMENT_EXPIRE, &callback_erreur, ST_ERREUR },
     {ST_ALLUMES, EV_CMD_1, NULL, ST_ALLUMES},
 
-    /*ACQUITTES*/
+    /* ACQUITTES */
     {ST_ACQUITTES, EV_CMD_0, &callback_eteindre_feux, ST_ETEINTS},
     {ST_ACQUITTES, EV_CMD_1, &callback_acquitter, ST_ACQUITTES},
 
-    /*ERREUR*/
+    /* ERREUR */
     { ST_ANY, EV_ERR, &callback_erreur, ST_TERM}
 };
 
@@ -107,6 +124,7 @@ tTransition trans[] = {
  * @brief Détermine le prochain événement à traiter.
  *
  * Lit les entrées de commande et retourne l'événement correspondant en fonction de l'état actuel.
+ * L'ordre des conditions est important dans la logique de la FSM.
  *
  * @param current_state État courant de la FSM.
  * @return Evénement à traiter.
@@ -126,19 +144,19 @@ int get_next_event(int current_state)
     boolean_t commande_feux_croisement = get_commande_feux_croisement();
     boolean_t commande_feux_route = get_commande_feux_route();
 
-    boolean_t commande_feux_any = (commande_feux_position == 1)
-                                 || (commande_feux_croisement == 1)
-                                 || (commande_feux_route == 1);
+    boolean_t commande_feux_any = (commande_feux_position == CMD_ACTIVEE)
+                                 || (commande_feux_croisement == CMD_ACTIVEE)
+                                 || (commande_feux_route == CMD_ACTIVEE);
 
-    if(commande_feux_any == 0) {
+    if(commande_feux_any == CMD_ETEINTE) {
         return EV_CMD_0;
     }
 
-    if(commande_feux_any == 1) {
+    if(commande_feux_any == CMD_ACTIVEE) {
 
         if(current_state == ST_ALLUMES) {
 
-            if(get_acquittement_fsm_feux_classiques == 1) {
+            if(get_acquittement_fsm_feux_classiques == CMD_ACTIVEE) {
                 return EV_ACQUITTEMENT_RECU;
             }
 
